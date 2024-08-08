ea1d7a98936d1325b1867236a72aca447b7ca727
Akula Satya Sesha Sai <112323924+satya1410@users.noreply.github.com>
2024-08-08 17:05:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+document.querySelector('.login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Implement your login logic here
+    alert('Login button clicked!');
+});
